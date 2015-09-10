f9a3b73032f7bed56d80e1d4914385a2ec11322f
Rickard Närström <rickard@narstrom.se>
2015-09-10 20:02:13 +0200

    Move build configuration into a sepparate file

---
+++ config.mk
@@ -0,0 +1,19 @@
+#
+# Myr C Utils library
+#
+# Written 2015 by Rickard Närström <rickard@narstrom.se>
+#
+# To the extent possible under law, the author(s) have dedicated all copyright
+# and related and neighboring rights to this software to the public domain worldwide.
+# This software is distributed without any warranty.
+#
+# You should have received a copy of the CC0 Public Domain Dedication along
+# with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+CC=clang
+CCLD=clang
+RM=rm -f
+AR=ar
+RANLIB=ranlib
+
+CFLAGS=-Weverything -Werror -g
