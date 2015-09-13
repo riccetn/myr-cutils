45eb998b2e1874fef19320062ceabbfbc495e1a4
Rickard Närström <rickard@narstrom.se>
2015-09-13 08:18:40 +0200

    Add documentetion for functions in <myr/err.h>

--- include/myr/err.h
+++ include/myr/err.h
@@ -12,14 +12,91 @@
  */
 #ifndef ERR_H
 #define ERR_H
+/**
+ * \file myr/err.h
+ */
 
 #include "compiler.h"
 
+/**
+ * \fn void NORETURN fatal(int ec, const char *format, ...)
+ * \brief Print a error message and exit.
+ *
+ * Prints an error message to <code>stderr</code> and exit. The error message
+ * printed will include the message specefied.
+ *
+ * \param ec exit code
+ * \param format printf-style format string
+ * \param ... parameters for format string
+ * \return This function never returns
+ */
 void NORETURN fatal(int ec, const char *format, ...) FORMAT(printf, 2, 3);
+
+/**
+ * \fn void NORETURN fatal_errno(int ec, const char *format, ...)
+ * \brief Print a error message and exit.
+ *
+ * Prints an error message to <code>stderr</code> and exit. The error message
+ * printed will include the message specefied and a message indicating the current
+ * <code>errno</code>.
+ *
+ * \param ec exit code
+ * \param format printf-style format string
+ * \param ... parameters for format string
+ * \return This function never returns
+ */
 void NORETURN fatal_errno(int ec, const char *format, ...) FORMAT(printf, 2, 3);
+
+/**
+ * \fn void NORETURN bug(const char *format, ...)
+ * \brief Print a error message and core dump.
+ *
+ * Prints an error message to <code>stderr</code> and core dump. The error
+ * message will include the message specefied.
+ *
+ * \param format printf-style format string
+ * \param ... parameters for format string
+ * \return This function never returns
+ */
 void NORETURN bug(const char *format, ...) FORMAT(printf, 1, 2);
+
+/**
+ * \fn void NORETURN bug_errno(const char *format, ...)
+ * \brief Print a error message and core dump.
+ *
+ * Prints an error message to <code>stderr</code> and core dump. The error
+ * message will include the message specefied and a message indicateg the current
+ * <code>errno</code>.
+ *
+ * \param format printf-style format string
+ * \param ... parameters for format string
+ * \return This function never returns
+ */
 void NORETURN bug_errno(const char *format, ...) FORMAT(printf, 1, 2);
+
+/**
+ * \fn void warn(const char *format, ...)
+ * \brief Print a error message.
+ *
+ * Prints an error message to <code>stderr</code>. The error
+ * message will include the message specefied.
+ *
+ * \param format printf-style format string
+ * \param ... parameters for format string
+ */
 void warn(const char *format, ...) FORMAT(printf, 1, 2);
+
+/**
+ * \fn void warn_errno(const char *format, ...)
+ * \brief Print a error message.
+ *
+ * Prints an error message to <code>stderr</code>. The error message will
+ * include the message specefied and a message indicateg the current
+ * <code>errno</code>.
+ *
+ * \param format printf-style format string
+ * \param ... parameters for format string
+ */
 void warn_errno(const char *format, ...) FORMAT(printf, 1, 2);
 
 #endif
